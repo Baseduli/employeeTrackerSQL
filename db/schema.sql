7d5bc4d6e45883eb2c3f2057ab47ee48c7854a1b
baseduli <ulisesvivar123@gmail.com>
2023-11-14 21:28:10 -0500

    created db folder> created schema.sql> schema will create employee_db and use it. Created departments, employees, and role tables. Started working on the server.js file. Will circle back to complete

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+--Delete the database if it already exists and creates a new one
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+-- sets employees_db as the current database
+USE employees_db;
+
+-- Create tables with the department fields and a primary key
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create tables with the role fields and a foreign key that references the department table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create tables with the employee fields and a foreign key that references the role table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employees_db'
+    },
+    console.log('Connected to the employees_db database.')
+);
+
+app.post('/api/department', ({ body }, res) => {
+    const sql = `INSERT INTO department (name)
+                VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
